a2d7bfa7e20274297db21c20f15b60b3d285b079
A713\dam1 <kevinslayer186@gmail.com>
2026-01-07 21:19:05 +0100

    no message

---
+++ Trimestre2/src/main/java/org/example/Recursividad.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Recursividad {
+    //Primer mandamiento: siempre tiene que haber dos caminos uno que se llame a si mismo y otro para salir
+
+}
---
+++ Trimestre2/src/main/java/org/example/Recursividad2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Recursividad2 {
+    static int limite = 10;
+    public static void main(String[] args){
+
+        imprimir(1);
+    }
+    public static void imprimir(int inicio){
+        if (inicio > limite){
+            return;
+        }else{
+            System.out.println(inicio);
+            imprimir(inicio+1);
+        }
+    }
+}
